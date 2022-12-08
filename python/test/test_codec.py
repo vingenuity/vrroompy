f642cc4497659702ed5ab26582b1d0c194b7da06
Vincent Kocks <engineering@vingenuity.net>
2022-12-08 15:35:43 -0600

    Completes Codec unit tests.

--- python/test/test_codec.py
+++ python/test/test_codec.py
@@ -14,12 +14,42 @@ class TestCodec(unittest.TestCase):
     Unit tests the class vrroompy.Codec.
     """
 
+    def test_decode_response_output(self):
+        # String type, single output
+        decoded_response = Codec.decode_response(
+            b'opmode 4\r\n',
+            "opmode",
+            ["[0-4]"],
+            [str])
+        self.assertTrue(decoded_response == ["4"])
+
+        # Custom type, multiple outputs
+        decoded_response = Codec.decode_response(
+            b'insel 0 4\r\n',
+            "insel",
+            [Input.pattern(), Input.pattern()],
+            [Input.from_string, Input.from_string])
+        self.assertTrue(decoded_response == [Input.RX0, Input.FOLLOW])
+
+    def test_decode_response_raises(self):
+        # Asserts when value pattern does not match response
+        with self.assertRaises(ValueError):
+            Codec.decode_response(
+                b'opmode 5\r\n',
+                "opmode",
+                ["[0-4]"],
+                [str])
+
     def test_decode_raw(self):
         decoded_response = Codec.decode_response_raw(b'opmode 4\r\n')
         self.assertTrue(decoded_response == "opmode 4")
 
         decoded_response = Codec.decode_response_raw(b'insel 3 0\r\n')
         self.assertTrue(decoded_response == "insel 3 0")
+
+    def test_encode_get(self):
+        encoded_command = Codec.encode_command_get("opmode")
+        self.assertTrue(encoded_command == b'get opmode\n')
 
     def test_encode_raw(self):
         # Caller provides terminator for us
@@ -29,10 +59,6 @@ class TestCodec(unittest.TestCase):
         # Caller forgets to provide terminator
         encoded_command = Codec.encode_command_raw("set insel 2 4")
         self.assertTrue(encoded_command == b'set insel 2 4\n')
-
-    def test_encode_get(self):
-        encoded_command = Codec.encode_command_get("opmode")
-        self.assertTrue(encoded_command == b'get opmode\n')
 
     def test_encode_set(self):
         # Single value, str type
